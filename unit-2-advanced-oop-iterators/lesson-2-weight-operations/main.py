41ad0f3dabadd60cce40eb8a8932b5cf1e692060
bobo258 <bobo258@merry>
2019-05-21 18:04:53 +0800

    kComplete lesson 2 practice

--- unit-2-advanced-oop-iterators/lesson-2-weight-operations/main.py
+++ unit-2-advanced-oop-iterators/lesson-2-weight-operations/main.py
@@ -1,2 +1,30 @@
+# 1 pound = 0.4536 kg
+
 class Weight(object):
-    pass
+    def __init__(self, kilograms=None, pounds=None):
+        if not any([kilograms, pounds]):
+            raise ValueError()
+        self._kilograms = kilograms
+        self._pounds = pounds
+
+    @property
+    def kilograms(self):
+        return self._kilograms or self._pounds * 0.4536
+
+    @property
+    def pounds(self):
+        return self._pounds or self._kilograms / 0.4536
+
+    def __add__(self, other):
+        if self._kilograms:
+            return Weight(kilograms=(self.kilograms + other.kilograms))
+        return Weight(pounds=(self.pounds + other.pounds))
+
+
+if __name__ == '__main__':
+    w1 = Weight(kilograms=80)
+    w2 = Weight(pounds=46)
+    w3 = w1 + w2
+
+    print(w3.kilograms)  # 100.90
+    print(w3.pounds)  # 222.0
