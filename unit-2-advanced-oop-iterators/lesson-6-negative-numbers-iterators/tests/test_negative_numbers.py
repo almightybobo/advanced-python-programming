27304e4d96d7383a799f21a7f8c6e45c1a833827
bobo258 <bobo258@merry>
2019-05-23 11:18:03 +0800

    Complete lesson 6 practice

--- unit-2-advanced-oop-iterators/lesson-6-negative-numbers-iterators/tests/test_negative_numbers.py
+++
@@ -1,7 +0,0 @@
-def test_negative_numbers():
-    iterator = iter(NegativeNumbersIterator())
-
-    assert next(iterator) == 0
-    assert next(iterator) == -1
-    assert next(iterator) == -2
-    assert next(iterator) == -3
