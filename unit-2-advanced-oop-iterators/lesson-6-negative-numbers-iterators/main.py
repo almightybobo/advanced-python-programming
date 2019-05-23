61f8bff838e8577dc2d644f9bb6b28f5d28fe904
bobo258 <bobo258@merry>
2019-05-23 11:17:31 +0800

    Complete lesson 6 practice

--- unit-2-advanced-oop-iterators/lesson-6-negative-numbers-iterators/main.py
+++ unit-2-advanced-oop-iterators/lesson-6-negative-numbers-iterators/main.py
@@ -1,2 +1,23 @@
 class NegativeNumbersIterator(object):
-    pass
+    def __init__(self):
+        self.next_number = 0
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        number = self.next_number
+        self.next_number -= 1
+        return number
+
+    next = __next__
+
+if __name__ == '__main__':
+    iterator = iter(NegativeNumbersIterator())
+
+    print(next(iterator))
+    print(next(iterator))
+    print(next(iterator))
+    print(next(iterator))
+    print(next(iterator))
+    print(next(iterator))
