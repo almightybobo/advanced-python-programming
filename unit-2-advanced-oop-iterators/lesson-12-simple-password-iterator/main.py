bcd8fc7dd60ad4c92b741a17c247faf9e53d8de4
bobo258 <bobo258@merry>
2019-05-23 17:38:28 +0800

    Complete lesson 12 practice

--- unit-2-advanced-oop-iterators/lesson-12-simple-password-iterator/main.py
+++ unit-2-advanced-oop-iterators/lesson-12-simple-password-iterator/main.py
@@ -1,6 +1,27 @@
-class SimplePasswordGenerator(object):
-    def __iter__(self):
-        pass
+import random
+import string
 
-    def next(self):  # use __next__ in Python 3.x
-        pass
+class SimplePasswordGenerator(object):
+    def __init__(self, available_chars=None, length=8):
+        if not available_chars:
+            available_chars = list(string.digits + string.ascii_letters + string.punctuation)
+        self.available_chars = available_chars
+        self.length = length
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        result = []
+        for i in range(self.length):
+            result.append(random.choice(self.available_chars))
+
+        return ''.join(result)
+
+if __name__ == '__main__':
+    pass_generator = SimplePasswordGenerator()
+    print(next(pass_generator))
+    print(next(pass_generator))
+
+    pass_generator = SimplePasswordGenerator(available_chars=['a', 'b'], length=4)
+    print(next(pass_generator))
