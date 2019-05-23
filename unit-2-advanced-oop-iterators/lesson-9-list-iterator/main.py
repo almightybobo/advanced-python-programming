0199df2c726280abc40c1fdb8a5f222519b04aec
bobo258 <bobo258@merry>
2019-05-23 15:39:33 +0800

    Complete lesson 9 practice

--- unit-2-advanced-oop-iterators/lesson-9-list-iterator/main.py
+++ unit-2-advanced-oop-iterators/lesson-9-list-iterator/main.py
@@ -1,6 +1,23 @@
 class ListIterator(object):
-    pass
+    def __init__(self, number_list):
+        self.number_list = number_list
+        self.index = 0
 
+    def __iter__(self):
+        return self
 
-# for elem in ListIterator([3, 2, 1]):
-#     print(elem)
+    def __next__(self):
+        index = self.index
+        self.index += 1
+        if self.index > len(self.number_list):
+            exit()
+            # better answer ( below )
+            # raise StopIteration
+
+        return self.number_list[index]
+
+    next = __next__
+
+if __name__ == '__main__':
+    for elem in ListIterator([3, 2, 1]):
+        print(elem)
--- unit-2-advanced-oop-iterators/lesson-9-list-iterator/tests/test_with_empty_list.py
+++
@@ -1,6 +0,0 @@
-import pytest
-
-def test_with_empty_list():
-    it = iter(ListIterator([]))
-    with pytest.raises(StopIteration):
-        next(it)
