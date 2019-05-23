0e3aea21e51d54c86975e5a4580a302531ae42f8
bobo258 <bobo258@merry>
2019-05-23 11:29:43 +0800

    Complete lesson 7 practice

--- unit-2-advanced-oop-iterators/lesson-7-count-up-to-x/main.py
+++ unit-2-advanced-oop-iterators/lesson-7-count-up-to-x/main.py
@@ -1,2 +1,26 @@
 class CountUpToIterator(object):
-    pass
+    def __init__(self, up_to):
+        self.number = 0
+        self.up_to = up_to
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        number = self.number
+        self.number += 1
+        if number > self.up_to:
+            raise StopIteration()
+        return number
+
+    next = __next__
+
+if __name__ == '__main__':
+    iterator = iter(CountUpToIterator(up_to=5))
+    print(next(iterator)) # 0
+    print(next(iterator)) # 1
+    print(next(iterator)) # 2
+    print(next(iterator)) # 3
+    print(next(iterator)) # 4
+    print(next(iterator)) # 5
+    print(next(iterator)) # StopIteration() Raised!
