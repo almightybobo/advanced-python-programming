9a28586bda4e6b84994ec912c587e4d3dc092c7c
bobo258 <bobo258@merry>
2019-05-23 13:34:02 +0800

    Complete lesson 8 practice

--- unit-2-advanced-oop-iterators/lesson-8-prime-numbers-iterators/main.py
+++ unit-2-advanced-oop-iterators/lesson-8-prime-numbers-iterators/main.py
@@ -1,11 +1,49 @@
 class PrimeNumbersIterator(object):
     def __iter__(self):
-        pass
+        self.next_number = 2
+        return self
 
     def _is_prime(self, number):
-        pass
+        if number <= 1:
+            return False
+        else:
+            for i in range(2, number):
+                if number % i == 0:
+                    return False
+            return True
 
     def __next__(self):
-        pass
+        while(1):
+            number = self.next_number
+            if self._is_prime(number):
+                self.next_number += 1
+                return number
+            else:
+                self.next_number += 1
+
+        # other solution
+        '''
+        number = self.next_number
+        while not self._is_prime(number):
+            number += 1
+
+        self.next_number = number + 1
+        return number
+        '''
 
     next = __next__
+
+
+if __name__ == '__main__':
+    obj = PrimeNumbersIterator()
+    print(obj._is_prime(3))  # True
+    print(obj._is_prime(199))  # True
+    print(obj._is_prime(20))  # False
+
+    iterator = iter(PrimeNumbersIterator())
+
+    print(next(iterator))  # 2
+    print(next(iterator))  # 3
+    print(next(iterator))  # 5
+    print(next(iterator))  # 7
+    print(next(iterator))  # 11
