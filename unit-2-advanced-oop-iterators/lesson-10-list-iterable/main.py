069aff2603064195960a7d36c988b0f0c5e70953
bobo258 <bobo258@merry>
2019-05-23 15:51:47 +0800

    Complete lesson 10 practice

--- unit-2-advanced-oop-iterators/lesson-10-list-iterable/main.py
+++ unit-2-advanced-oop-iterators/lesson-10-list-iterable/main.py
@@ -1,15 +1,31 @@
 class ListIterable(object):
+    def __init__(self, number_list):
+        self.number_list = number_list
+        self.index = 0
+
     def __next__(self):
-        pass
+        index = self.index
+        self.index += 1
+        if self.index > len(self.number_list):
+            raise StopIteration
+
+        return self.number_list[index]
+
+    next = __next__
 
 
 class ListIterator(object):
-    def __iter__(self):
-        pass
+    def __init__(self, number_list):
+        self.number_list = number_list
 
-# iterator = ListIterator([3, 2, 1])
-# for elem in iterator:
-#     print(elem)
-# print("====================")
-# for elem in iterator:
-#     print(elem)
+    def __iter__(self):
+        return ListIterable(self.number_list)
+
+
+if __name__ == '__main__':
+    iterator = ListIterator([3, 2, 1])
+    for elem in iterator:
+        print(elem)
+    print("====================")
+    for elem in iterator:
+        print(elem)
