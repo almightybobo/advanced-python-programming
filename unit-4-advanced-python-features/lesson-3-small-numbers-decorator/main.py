973b655cf7919c0dc153ddf7c9ee3b765651a8f4
bobo258 <bobo258@merry>
2019-05-24 18:08:52 +0800

    Complete lesson 3 practice

--- unit-4-advanced-python-features/lesson-3-small-numbers-decorator/main.py
+++ unit-4-advanced-python-features/lesson-3-small-numbers-decorator/main.py
@@ -1,2 +1,16 @@
 def small_numbers(fn):
-    pass
+    def wrapper(*args, **kwargs):
+        for arg in args:
+            if type(arg) in [int, float]:
+                if arg >= 100:
+                    raise ValueError("Error")
+        return fn(*args, **kwargs)
+    return wrapper
+
+
+@small_numbers
+def my_func(number_param, string_param):
+      pass
+
+my_func(99, "Hello")
+my_func(101, "Oh no!")
